0c06f6477a978ef872cd45941e479c7f2ba081d8
MalMWare <malkelleymartin@gmail.com>
2023-06-25 23:40:29 -0700

    server.js

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+//modules and globals
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const mongoose = require('mongoose')
+
+//configuration and middleware
+app.use(cors({
+    origin: '*', 
+    credentials: true,
+    optionSuccessStatus: 200,
+}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
+const PORT = process.env.PORT
+const MONGO_URI = process.env.MONGO_URI
+mongoose.set({strictQuery: true})
+try {
+    mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    console.log('DATABASE CONNECTED')
+} catch (err) {
+    console.log(err)
+}
+
+//controllers and routes
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Dungeon!'})
+})
+
+app.get('*', (req, res) => {
+    res.status(404).json({ message: 'endpoint data not found' })
+})
+
+//listening for connections
+app.listen(PORT, () => {
+    console.log(`🎸 Rockin' on port: ${PORT}`)
+})
+
+module.exports = app
