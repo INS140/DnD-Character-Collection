3cfb912385f59ab7de48c1f7ff910889e7ccff36
MalMWare <malkelleymartin@gmail.com>
2023-06-27 19:52:10 -0700

    fleshed out controllers

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,52 +1,93 @@
 const users = require("express").Router()
 const UserData = require('../Models/user.js')
 
+//FIND ALL USERS
 users.get('/', async (req, res) => {
     try {
         const users = await UserData.find()
         return res.status(200).json(users)
     }
-    catch {
+    catch (err) {
+        console.log(err)
         res.status(500).json({ message: 'Server error' })
     }
 })
 
+//FIND SPECIFIC USER
 users.get('/:id', async (req, res) => {
     try {
-        const user = await UserData.findById(req.params.id)
-        return res.status(200).json(user)
+        const foundUser = await UserData.findById(req.params.id)
+        return res.status(200).json(foundUser)
     }
-    catch {
+    catch (err) {
+        console.log(err)
         res.status(500).json({ message: 'Server error' })
     }
 })
 
+//CREATE USER
 users.post('/', async (req, res) => {
     try {
         const newUser = await UserData.create(req.body)
-        return res.status(201).json(newUser)
+        return res.status(201).json({
+            message: 'Successfully insert a new user',
+            data: newUser
+        })
     }
-    catch {
+    catch (err) {
+        console.log(err)
         res.status(500).json({ message: 'Server error' })
     }
 })
 
+//USER ERROR
+user.post('/', (req, res) => {
+    try {
+        res.redirect("/")
+    }
+    catch (err) {
+        if (err && err.name == 'ValidationError') {
+          for (var field in err.errors) {
+            message += `${field} was ${err.errors[field].value}.`
+            message += `${err.errors[field].message}`
+          }
+          res.render('users/new', ({ message: 'Validation error' }))
+        }
+        else {
+            res.status(500).json({ message: 'Server error' })
+        }   
+    }
+})
+
+
+//UPDATE USER INFORMATION
 users.put('/:id', async (req, res) => {
     try {
-        const updatedUser = await UserData.findByIdAndUpdate(req.params.id, req.body)
-        return res.status(200).json(updatedUser)
+        const updatedUser = await UserData.findByIdAndUpdate(req.params.id, req.body, {
+            new: true
+        })
+        return res.status(200).json({
+            message: 'Successfully updated user',
+            data: updatedUser
+        })
     }
-    catch {
+    catch (err) {
+        console.log(err)
         res.status(500).json({ message: 'Server error' })
     }
 })
 
+//DELETE A USER
 users.delete('/:id', async (req, res) => {
     try {
-        const removedUser = await UserData.findByIdAndDelete(req.params.id)
-        return res.status(200).json(removedUser)
+        const deletedUser = await UserData.findByIdAndDelete(req.params.id)
+        return res.status(200).json({
+            message: `Successfully deleted user`,
+            data: deletedUser
+        })
     }
-    catch {
+    catch (err) {
+        console.log(err)
         res.status(500).json({ message: 'Server error' })
     }
 })
---
+++ backend/models/users_model.js
@@ -0,0 +1,10 @@
+// Require mongoose
+const mongoose = require('mongoose');
+// Creating shorthand for the Schema constructor
+const { Schema } = mongoose;
+const Character = require('./character.js');
+
+//Schema
+const userSchema = new Schema({
+    userName: { type: String, required: true}
+})
