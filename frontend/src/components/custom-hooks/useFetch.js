295b0aea8476f66efd14ec1f0af4b8a24d9ca8e7
annielam0623 <annzhouyy@gmail.com>
2023-06-30 15:55:36 -0700

    nothing change but accidently press enter made one line down

--- frontend/src/components/custom-hooks/useFetch.js
+++ frontend/src/components/custom-hooks/useFetch.js
@@ -1,50 +1,51 @@
-export default function useFetch(baseUrl) { // this hook now requires that a baseurl be input as an arguemnet when called
+export default function useFetch(baseUrl) {
+  // this hook now requires that a baseurl be input as an arguemnet when called
   return {
-    get: async endpoint => {
+    get: async (endpoint) => {
       try {
-        const res = await fetch(baseUrl+endpoint)
-        return await res.json()
+        const res = await fetch(baseUrl + endpoint);
+        return await res.json();
       } catch (err) {
-        return err
+        return err;
       }
     },
     post: async (endpoint, body) => {
       try {
-        const res = await fetch(baseUrl+endpoint, {
+        const res = await fetch(baseUrl + endpoint, {
           method: "POST",
           headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
           },
-          body: JSON.stringify(body)
-        })
-        return await res.json()
+          body: JSON.stringify(body),
+        });
+        return await res.json();
       } catch (err) {
-        return err
+        return err;
       }
     },
     put: async (endpoint, body) => {
       try {
-        const res = await fetch(baseUrl+endpoint, {
-          method: 'PUT',
+        const res = await fetch(baseUrl + endpoint, {
+          method: "PUT",
           headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
           },
-          body: JSON.stringify(body)
-        })
-        return await res.json()
+          body: JSON.stringify(body),
+        });
+        return await res.json();
       } catch (err) {
-        return err
+        return err;
       }
     },
-    remove: async endpoint => {
+    remove: async (endpoint) => {
       try {
-        const res = await fetch(baseUrl+endpoint, {
-          method: 'DELETE'
-        })
-        return await res.json()
+        const res = await fetch(baseUrl + endpoint, {
+          method: "DELETE",
+        });
+        return await res.json();
       } catch (err) {
-        return err
+        return err;
       }
-    }
-  }
+    },
+  };
 }
