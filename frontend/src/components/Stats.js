a2722fd406259c84231e47641633d9cac54a737b
Zane Matero <materozane@gmail.com>
2023-06-29 21:02:43 -0700

    styling

--- frontend/src/components/Stats.js
+++ frontend/src/components/Stats.js
@@ -6,10 +6,14 @@
 export default function Stats( { stats } ) {
   return (
   <div class="stats">
+    <h1>General</h1>
+    <hr/>
     <h3>Health Points: {stats?.healthPoints}</h3>
     <h3>Armor Class: {stats?.armorClass}</h3>
     <h3>Max Health Points: {stats?.maxHp}</h3>
     <h3>Hit Dice: {stats?.hitDice}</h3>
+    <h1>Base Stats</h1>
+    <hr/>
     <h3>Speed: {stats?.speed}</h3>
     <h3>Proficiency: {stats?.proficiency}</h3>
     <h3>Strength: {stats?.strength}</h3>
@@ -18,8 +22,10 @@ export default function Stats( { stats } ) {
     <h3>Intelligence: {stats?.intelligence}</h3>
     <h3>Wisdom: {stats?.wisdom}</h3>
     <h3>Charisma: {stats?.charisma}</h3>
-    <h3>Skills: {stats?.skills}</h3>
-    <h3>Saving Throws: {stats?.savingThrows}</h3>
+    <h1>Saving Throws: {stats?.savingThrows}</h1>
+    <hr/>
+    <h1>Skills: {stats?.skills}</h1>
+    <hr/>
   </div>
   )
 }
