4f59b733fbfc06fca5ba44ad4f4b09ba964c856d
Zane Matero <materozane@gmail.com>
2023-06-29 20:41:45 -0700

    publish stats branch with template values

--- frontend/src/components/Stats.js
+++ frontend/src/components/Stats.js
@@ -3,8 +3,23 @@
 // group 2 base stats (ie STR, DEX, etc)
 // group 3 skills
 
-export default function Stats() {
-  return <>
-    PLACEHOLDER Stats
-  </>
+export default function Stats( { stats } ) {
+  return (
+  <div class="stats">
+    <h3>{stats.healthPoints}</h3>
+    <h3>{stats.armorClass}</h3>
+    <h3>{stats.maxHp}</h3>
+    <h3>{stats.hitDice}</h3>
+    <h3>{stats.speed}</h3>
+    <h3>{stats.proficiency}</h3>
+    <h3>{stats.strength}</h3>
+    <h3>{stats.dexterity}</h3>
+    <h3>{stats.constitution}</h3>
+    <h3>{stats.intelligence}</h3>
+    <h3>{stats.wisdom}</h3>
+    <h3>{stats.charisma}</h3>
+    <h3>{stats.skills}</h3>
+    <h3>{stats.savingThrows}</h3>
+  </div>
+  )
 }
