1991bfbf95a00f3190a4ab1e818a71dfba272368
Zane Matero <materozane@gmail.com>
2023-06-29 20:46:37 -0700

    add text display

--- frontend/src/components/Stats.js
+++ frontend/src/components/Stats.js
@@ -6,20 +6,20 @@
 export default function Stats( { stats } ) {
   return (
   <div class="stats">
-    <h3>{stats.healthPoints}</h3>
-    <h3>{stats.armorClass}</h3>
-    <h3>{stats.maxHp}</h3>
-    <h3>{stats.hitDice}</h3>
-    <h3>{stats.speed}</h3>
-    <h3>{stats.proficiency}</h3>
-    <h3>{stats.strength}</h3>
-    <h3>{stats.dexterity}</h3>
-    <h3>{stats.constitution}</h3>
-    <h3>{stats.intelligence}</h3>
-    <h3>{stats.wisdom}</h3>
-    <h3>{stats.charisma}</h3>
-    <h3>{stats.skills}</h3>
-    <h3>{stats.savingThrows}</h3>
+    <h3>Health Points: {stats?.healthPoints}</h3>
+    <h3>Armor Class: {stats?.armorClass}</h3>
+    <h3>Max Health Points: {stats?.maxHp}</h3>
+    <h3>Hit Dice: {stats?.hitDice}</h3>
+    <h3>Speed: {stats?.speed}</h3>
+    <h3>Proficiency: {stats?.proficiency}</h3>
+    <h3>Strength: {stats?.strength}</h3>
+    <h3>Dexterity: {stats?.dexterity}</h3>
+    <h3>Constitution: {stats?.constitution}</h3>
+    <h3>Intelligence: {stats?.intelligence}</h3>
+    <h3>Wisdom: {stats?.wisdom}</h3>
+    <h3>Charisma: {stats?.charisma}</h3>
+    <h3>Skills: {stats?.skills}</h3>
+    <h3>Saving Throws: {stats?.savingThrows}</h3>
   </div>
   )
 }
