06fcc1888d1a120c644eefc8e451fbb50cf0e7f0
annielam0623 <annzhouyy@gmail.com>
2023-07-01 11:03:32 -0700

    fixed some error

--- frontend/src/components/CharacterView.js
+++ frontend/src/components/CharacterView.js
@@ -3,24 +3,26 @@
 // fetch individual character data
 import { useEffect, useState } from "react";
 import { Outlet, useParams } from "react-router-dom";
-import useFetch from './custom-hooks/useFetch'
+import useFetch from "./custom-hooks/useFetch";
 
 export default function CharacterView() {
-  const { get } = useFetch('http://localhost:5000')
+  const { get } = useFetch("http://localhost:5000");
 
-  const [character, setCharacter] = useState(null)
+  const [character, setCharacter] = useState(null);
 
-  const { id } = useParams()
-  
+  const { id } = useParams();
+
   useEffect(() => {
     (async () => {
-      const data = await get(`/characters/${id}`)
-      console.log(data)
-      setCharacter(data)
-    })()
-  }, [])
+      const data = await get(`/characters/${id}`);
+      console.log(data);
+      setCharacter(data);
+    })();
+  }, [get, id]);
 
-  return <div>
-    <Outlet context={{character: character}} />
-  </div>
+  return (
+    <div>
+      <Outlet context={{ character: character }} />
+    </div>
+  );
 }
