deef0e9b27541d39104bc43ff0465697ff45d511
INS140 <IsaacNSaettel@gmail.com>
2023-07-01 06:26:05 -0700

    refactor

--- frontend/src/components/CharacterGrid.js
+++ frontend/src/components/CharacterGrid.js
@@ -14,42 +14,13 @@ export default function CharacterGrid() {
   useEffect(() => {
     (async () => {
       try {
-        const foundCharacters = await get('/characters', localStorage.getItem('token'))
-
-        console.log(foundCharacters)
-  
-        setCharacters(!foundCharacters ? mock : foundCharacters)
+        const data = await get('/characters', localStorage.getItem('token'))
+        setCharacters(data)
       } catch (err) {
         console.log(err)
       }
     })()
   }, [])
-
-  const mock = [{
-    id: 1,
-    name: "Milguaar",
-    image: "https://as2.ftcdn.net/v2/jpg/05/62/86/97/1000_F_562869742_exRn1yPPpH3LQ7QQ4bReEpcNZTfIQerj.jpg",
-    classType: "High Elf Wizard",
-    level: 1000000
-  }, {
-    id: 2,
-    name: "Marth",
-    image: "https://t4.ftcdn.net/jpg/03/95/07/45/360_F_395074522_KDR8Luffl4Rqh1I1ElQAV9e9W1EL1EJB.jpg",
-    classType: "Ogre Brute",
-    level: 93
-  }, {
-    id: 3,
-    name: "Kennikith The Cunning",
-    image: "https://assetsio.reedpopcdn.com/the-modern-day-thief-reboot-that-never-was-1440764496721.jpg?width=1200&height=900&fit=crop&quality=100&format=png&enable=upscale&auto=webp",
-    classType: "Thief Pick-Pocketer",
-    level: 7021
-  }, {
-    id: 4,
-    name: "Shaun",
-    image: "https://t3.ftcdn.net/jpg/05/73/92/02/360_F_573920251_veCkGPsuqXwZ8pXOl1WulBjho09F3g7u.jpg",
-    classType: "Great Knight",
-    level: 487
-  }]
 
   return <div className="grid-container">
     { !currentUser
