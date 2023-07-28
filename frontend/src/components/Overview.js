d71f7d4146cd87987c8a6f4adce9b516337fbe7a
MalMWare <malkelleymartin@gmail.com>
2023-07-27 22:12:09 -0700

    added ability score mods to overview

--- frontend/src/components/Overview.js
+++ frontend/src/components/Overview.js
@@ -1,7 +1,9 @@
 import { useOutletContext } from "react-router-dom"
+import { scoreToMod } from "../helper-functions"
 
 export default function Overview() {
   const { character } = useOutletContext()
+  const stats = ['str', 'dex', 'con', 'int', 'wis', 'cha']
 
   return <div className="overview">
     <h1>Overview</h1>
@@ -10,22 +12,23 @@ export default function Overview() {
     <h2>Class: {character.classType}</h2>
     <h2>Level: {character.level}</h2>
     <hr />
-    <h2>General Stats</h2>
+    {/* <h2>General Stats</h2>
     <div className="genStats">
       <h3 className="secondary">HP<span>{character.hp}</span></h3>
       <h3 className="secondary">AC<span>{character.ac}</span></h3>
       <h3 className="secondary">Max HP<span>{character.maxHp}</span></h3>
       <h3 className="secondary">Hit Dice<span>{character.hitDice}</span></h3>
     </div>
-    <hr />
+    <hr /> */}
     <h2>Ability Scores</h2>
-    <div className="baseStats">
-      <h3 className="secondary">STR<span>{character.str}</span></h3>
-      <h3 className="secondary">DEX<span>{character.dex}</span></h3>
-      <h3 className="secondary">CON<span>{character.con}</span></h3>
-      <h3 className="secondary">INT<span>{character.int}</span></h3>
-      <h3 className="secondary">WIS<span>{character.wis}</span></h3>
-      <h3 className="secondary">CHA<span>{character.cha}</span></h3>
+    <div className="baseOverStats" >
+      {stats.map(stat => {
+        return <div className="secondary abSc">
+          <h3>{stat.toUpperCase()}</h3>
+          <p>{character[stat]}</p>
+          <p className="mod">{scoreToMod(character[stat])}</p>
+        </div>
+      })}
     </div>
     <hr />
     <div className="description">
