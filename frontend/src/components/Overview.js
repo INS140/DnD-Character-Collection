98929671d6858aad811950053d776174419cd43c
MalMWare <malkelleymartin@gmail.com>
2023-07-27 22:18:53 -0700

    removed gen stats from overview

--- frontend/src/components/Overview.js
+++ frontend/src/components/Overview.js
@@ -12,14 +12,6 @@ export default function Overview() {
     <h2>Class: {character.classType}</h2>
     <h2>Level: {character.level}</h2>
     <hr />
-    {/* <h2>General Stats</h2>
-    <div className="genStats">
-      <h3 className="secondary">HP<span>{character.hp}</span></h3>
-      <h3 className="secondary">AC<span>{character.ac}</span></h3>
-      <h3 className="secondary">Max HP<span>{character.maxHp}</span></h3>
-      <h3 className="secondary">Hit Dice<span>{character.hitDice}</span></h3>
-    </div>
-    <hr /> */}
     <h2>Ability Scores</h2>
     <div className="baseOverStats" >
       {stats.map(stat => {
