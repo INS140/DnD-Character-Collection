cee72a96c4875636bee811115513cd6d8ad4bdd4
INS140 <IsaacNSaettel@gmail.com>
2023-08-01 05:33:02 -0700

    update ac, hit dice, and speed

--- frontend/src/components/Stats.js
+++ frontend/src/components/Stats.js
@@ -4,9 +4,61 @@ import Skills from "./Skills"
 import AbilityScores from "./AbilityScores"
 import PassiveStat from "./PassiveStat"
 import Initiative from "./Initiative"
+import Input from "./ui-kit/Input"
+import useFormHandler from "./custom-hooks/useFormHandler"
+import useFetch from "./custom-hooks/useFetch"
 
 export default function Stats() {
+  const { put } = useFetch('https://dnd-character-collection-backend.vercel.app')
+
   const { character, setCharacter } = useOutletContext()
+
+  const { inputs, handleChange, setInputs } = useFormHandler({
+    ac: character.ac,
+    hitDice: character.hitDice,
+    speed: character.speed
+  })
+
+  const handleNumberChange = e => {
+    const { name, value } = e.target
+
+    const max = name === 'ac' ? 50 : 999
+    const min = name ===  'ac' ? -50 : -999
+
+    const num = value === ''
+      ? ''
+      : Number(value) > max
+        ? max
+        : Number(value) < min
+          ? min
+          : Number(value)
+
+    setInputs({...inputs, [name]: num})
+  }
+
+  const handleFocusOut = async e => {
+    const { name, value } = e.target
+
+    const { prof, init, ...charData } = character
+
+    if (value === '') {
+      setInputs({...inputs, [name]: character[name]})
+      return
+    }
+
+    const newValue = name === 'hitDice' ? value : Number(value)
+
+    const updatedCharacter = {
+      ...charData,
+      [name]: newValue
+    }
+
+    await put(`/characters/${character.id}`, updatedCharacter)
+
+    setCharacter({...updatedCharacter, prof, init})
+
+    setInputs({...inputs, [name]: newValue})
+  }
 
   return <div className="stats">
     <h1>Stats</h1>
@@ -14,18 +66,37 @@ export default function Stats() {
     <div className="genStats">
       <div className="secondary stat ac">
         <h3>AC</h3>
-        <p>{character.ac}</p>
+        <Input
+          type="number"
+          name="ac"
+          value={inputs.ac}
+          onChange={handleNumberChange}
+          onBlur={handleFocusOut}
+          pattern="\d*"
+        />
       </div>
       <HPDisplay character={character} setCharacter={setCharacter} />
       <div className="secondary stat hd">
         <h3>Hit Dice</h3>
-        <p>{character.hitDice}</p>
+        <Input
+          name="hitDice"
+          value={inputs.hitDice}
+          onChange={handleChange}
+          onBlur={handleFocusOut}
+        />
       </div>
     </div>
     <div className="other secondary stat">
       <div>
         <h3>Speed</h3>
-        <p>{character.speed}</p>
+        <Input
+          type="number"
+          name="speed"
+          value={inputs.speed}
+          onChange={handleNumberChange}
+          onBlur={handleFocusOut}
+          pattern="\d*"
+        />
       </div>
       <Initiative character={character} setCharacter={setCharacter} />
       <div>
